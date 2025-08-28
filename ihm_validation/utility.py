29607fa625a9a2b6b5125365649d0560c8103a54
Arthur Zalevsky <aozalevsky@gmail.com>
2025-08-28 12:59:54 -0700

    do not apply formatting if pdbid is empty

--- ihm_validation/utility.py
+++ ihm_validation/utility.py
@@ -820,10 +820,10 @@ def format_wwpdb_id(pdbid: str) -> str:
     """Format all pdb ids to extended format"""
     output = pdbid
 
-    if len(pdbid) == 12:
+    if pdbid and len(pdbid) == 12:
         # We don't do any format checks
         output = pdbid.lower()
-    elif len(pdbid) == 4:
+    elif pdbid and len(pdbid) == 4:
         output = f"pdb_0000{pdbid.lower()}"
     else:
         logging.error(f"Wrong PDB ID: {pdbid}")
